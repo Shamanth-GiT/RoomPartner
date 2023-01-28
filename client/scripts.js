5daaed9ed615c5b824943765421a615fd5476bc1
Shamanth Kuthpadi <shamanth.kuthpadi2003@gmail.com>
2023-01-28 00:12:54 -0500

    Rearranged files to ensure that express.static() works

--- client/scripts.js
+++ client/scripts.js
@@ -1,3 +1,6 @@
+/*
+    Used to show activity status.
+*/
 function blinker(blink_elem){
     blink_elem.style.color= (blink_elem.style.color==='green'?'white':'green');
     setTimeout(() => blinker(blink_elem), 600);
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', express.static('client'));
 
-app.listen(port, () => console.log("Server started on port 3000"));
+app.listen(PORT, () => console.log("Server started on port 3000"));
