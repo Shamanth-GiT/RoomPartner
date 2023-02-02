a96d8b498698bd9d87b4429c502055c6f3bc3693
Shamanth Kuthpadi <shamanth.kuthpadi2003@gmail.com>
2023-02-02 17:03:05 -0500

    Database encrypted via mongoose-encryption

--- index.js
+++ index.js
@@ -10,9 +10,43 @@ app.use(express.static(path.join(__dirname, "/client")));
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Routes
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/views/register.html'));
-});
 
+app.route("/")
+    .get((req, res) => {
+        res.sendFile(path.join(__dirname, 'client/views/register.html'));
+    })
+    .post((req, res) => {
+        let obj = req.body;
+        console.log(obj);
+        const user = new User({
+            first_name: obj['first-name'],
+            last_name: obj['last-name'],
+            email: obj['email'],
+            password: obj['password']
+        });
+        user.save(err => {
+            if(err) console.log(err);
+            else res.sendFile(path.join(__dirname, 'client/views/home.html'));
+        });
+     });
+
+app.route("/login")
+    .get((req, res)=>{
+        res.sendFile(path.join(__dirname, 'client/views/login.html'));
+    })
+    .post((req, res)=>{
+        let obj = req.body;
+
+        const username = obj['username'];
+        const password = obj['password'];
+
+        User.findOne({email: username}, 
+            (err, foundUser) => {
+                if(err) console.log(err);
+                else if(foundUser){
+                    if(foundUser.password === password) res.sendFile(path.join(__dirname, 'client/views/home.html'));
+                } 
+            });
+    });
 
 app.listen(PORT, () => console.log("Server started on port 3000"));
--- users.js
+++ users.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
 mongoose.set('strictQuery', false);
 mongoose.connect("mongodb://localhost:27017/usersDB");
 
@@ -8,5 +10,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 module.exports = mongoose.model("User", userSchema);
