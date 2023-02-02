24ae5773c6b191f2881add3f97eb7cc132b147ed
Shamanth Kuthpadi <shamanth.kuthpadi2003@gmail.com>
2023-02-02 17:14:59 -0500

    added .env file for security

--- users.js
+++ users.js
@@ -11,7 +11,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 module.exports = mongoose.model("User", userSchema);
